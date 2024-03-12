f6babaa6b46cf3ddd709d7b9b8810de5f3d55d78
panamanaskvoz <panamanaskvoz@gmail.com>
2024-03-12 10:39:05 +0300

    6. Checkstyle [#4123 #28015]

---
+++ src/main/java/ru/job4j/array/Main.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        var input = "aab";
+        var count = 0;
+        String[] ary = input.split("");
+        var set = new HashSet<String>();
+        var builder = new StringBuilder();
+        for (int i = 0; i < ary.length; i++) {
+            if (set.contains(ary[i])) {
+                continue;
+            }
+            set.add(ary[i]);
+        }
+
+        for (var item : set) {
+            for (int j = 0; j < ary.length; j++) {
+                if (item.equals(ary[j])) {
+                    ++count;
+                }
+            }
+            if (count = =1) {
+                builder.append(item);
+            }
+            else {
+                builder.append(item + count);
+            }
+            count = 0;
+        }
+        System.out.println(builder);
+    }
+}
