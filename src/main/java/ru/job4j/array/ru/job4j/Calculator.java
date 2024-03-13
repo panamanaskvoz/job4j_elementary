22df46ea099aac2af8117a18ebc6eb5698f13c9d
panamanaskvoz <panamanaskvoz@gmail.com>
2024-03-13 06:17:13 +0300

    3.1. Переназначение переменной [#232354 #28059]

---
+++ src/main/java/ru/job4j/array/ru/job4j/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j.array.ru.job4j;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100;
+        System.out.println(size);
+        size =  size - 5;
+        System.out.println(size);
+    }
+}
