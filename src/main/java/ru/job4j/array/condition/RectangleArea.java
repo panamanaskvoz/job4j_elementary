804ccfddf8168602f19cfad230c23e9e47161eb5
panamanaskvoz <panamanaskvoz@gmail.com>
2024-03-15 11:43:02 +0300

    [#188 #28055]

---
+++ src/main/java/ru/job4j/array/condition/RectangleArea.java
@@ -0,0 +1,16 @@
+package ru.job4j.array.condition;
+
+public class RectangleArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        double result = s;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result);
+    }
+}
