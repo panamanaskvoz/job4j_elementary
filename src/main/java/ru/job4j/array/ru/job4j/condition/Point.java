b7acc9c17be0416ff4f3ed76c43a6c9475154b20
panamanaskvoz <panamanaskvoz@gmail.com>
2024-03-15 09:25:40 +0300

    3.1. Переназначение переменной [#232354 #28059]

---
+++ src/main/java/ru/job4j/array/ru/job4j/condition/Point.java
@@ -0,0 +1,17 @@
+package ru.job4j.array.ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double baby = x2 - x1;
+        double size = y2 - y1;
+        double firstBaby = Math.pow(baby, 2);
+        double firstSize = Math.pow(size, 2);
+        double result = firstBaby + firstSize;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(7, 31, 2, 19);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+    }
+}
