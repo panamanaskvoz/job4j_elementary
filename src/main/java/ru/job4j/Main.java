e2007bca870bd8fa30fb23efe9b3e78206b02055
panamanaskvoz <panamanaskvoz@gmail.com>
2024-03-16 20:03:39 +0300

    podval practic

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,16 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int calc = calc(1, 5);
+        System.out.println(calc);
+    }
+
+
+    public static void test() {
+        System.out.println("PODVAL RA");
+    }
+
+    public static int calc(int a, int b){
+        return a + b;
     }
 }
